90db73255d53ea25f71f78a895bdee6ae6fdf211
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2021-05-01 11:37:21 +0200

    nuevos efectos de sonido, musica del nivel

--- Assets/Scripts/DeathScripts/Level01Death.cs
+++ Assets/Scripts/DeathScripts/Level01Death.cs
@@ -7,6 +7,7 @@ public class Level01Death : MonoBehaviour
 {
     public AudioSource deathSound;
     public GameObject player;
+    public AudioSource levelSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +22,8 @@ public class Level01Death : MonoBehaviour
 
     private IEnumerator OnTriggerEnter(Collider col)
     {
+        levelSound.Stop();
+        print(this.name + " - " + col.name);
         GlobalLives.lives -= 1;
         deathSound.Play();
         player.transform.localScale -= new Vector3(0, (float)0.7, 0);
