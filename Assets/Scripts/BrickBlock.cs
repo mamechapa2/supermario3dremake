99a9257df6a7fe752dabe9a5dd6fbb999fd74a45
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2021-05-01 18:03:58 +0200

    brick block

--- Assets/Scripts/BrickBlock.cs
+++ Assets/Scripts/BrickBlock.cs
@@ -2,34 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestionBlock : MonoBehaviour
+public class BrickBlock : MonoBehaviour
 {
     public GameObject block;
     public GameObject deadBlock;
-    public GameObject blockObject;
     public Animator animationBlock;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    private IEnumerator OnTriggerEnter(Collider col)
+    private void OnTriggerEnter(Collider col)
     {
         print("aaaaaaaaaaaaaaaaaaaaaaaaaaa");
-        blockObject.SetActive(true);
         block.SetActive(false);
         deadBlock.SetActive(true);
-        yield return new WaitForSeconds(0.10F);
-        //animationBlock.enabled = false;
-        
+        StartCoroutine(delete());
+
+    }
+
+    private IEnumerator delete()
+    {
+        yield return new WaitForSeconds(0.40F);
+        animationBlock.enabled = false;
+        Destroy(deadBlock);
+
         print("Asdasdadada");
-        
     }
 }
