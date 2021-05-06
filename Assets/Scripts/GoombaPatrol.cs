fe88343d21c91f09d1b627703bfe4cf93b8e4f94
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2021-05-06 13:39:54 +0200

    goombapatrol y perseguir al jugador

--- Assets/Scripts/GoombaPatrol.cs
+++ Assets/Scripts/GoombaPatrol.cs
@@ -11,29 +11,68 @@ public class GoombaPatrol : MonoBehaviour
     public Transform[] moveSpots;
     private int randomSpot;
 
+    private Animator animator;
+    private bool caminar = true;
+
+    private GameObject player;
+    public float runningSpeed;
+    public float visionRadius;
+    private bool perseguir = false;
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponentInChildren<Animator>();
         waitTime = startWaitTime;
         randomSpot = Random.Range(0, moveSpots.Length);
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        Vector3 target = moveSpots[randomSpot].position;
+        float dist = Vector3.Distance(player.transform.position, transform.position);
+        if (dist < visionRadius)
+        {
+            target = player.transform.position;
+            perseguir = true;
+            caminar = false;
+        }
+        else
+        {
+            perseguir = false;
+            caminar = true;
+        }
+
+        if (caminar)
+        {
+            animator.speed = 1;
+            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            transform.LookAt(moveSpots[randomSpot]);
+        }
+        else
+        {
+            if (perseguir)
+            {
+                animator.speed = 1;
+                transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime * 2);
+                transform.LookAt(target);
+            }
+        }
 
         if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
         {
-            if (waitTime <= 0)
-            {
-                randomSpot = Random.Range(0, moveSpots.Length);
-                waitTime = startWaitTime;
-            }
-            else
-            {
-                waitTime -= Time.deltaTime;
-            }
+            StartCoroutine(esperar());
         }
+    }
+
+    IEnumerator esperar()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+        caminar = false;
+        animator.speed = 0;
+        yield return new WaitForSeconds(startWaitTime);
+        caminar = true;
+
     }
 }
