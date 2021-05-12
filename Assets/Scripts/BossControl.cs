047fd84e1a0da2e1b5d37511a3745f0b62853403
Alfonso Jose Arroyo Loaisa <ajal0004@red.ujaen.es>
2021-05-12 17:29:28 +0200

    assets explosiones, explosiones boss, animacion final, sonidos boss, vuelta al menu

--- Assets/Scripts/BossControl.cs
+++ Assets/Scripts/BossControl.cs
@@ -1,19 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossControl : MonoBehaviour
 {
     public GameObject prefab;
     public Transform startPos;
     public float waitTimeBeforeShooting;
+    private List<GameObject> fireballs;
 
     private bool shootNow = true;
     public static bool die = false;
+
+    public Animator animator;
+
+    //Para las explosiones
+    public GameObject explosion;
+    public float xmin;
+    public float xmax;
+    public float zmin;
+    public float zmax;
+    public float ymin;
+    public float ymax;
+    public AudioSource explosionAudio;
+    public AudioSource levelMusic;
+    public AudioSource endMusic;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fireballs = new List<GameObject>();
+        animator.SetFloat("speed", 0);
     }
 
     // Update is called once per frame
@@ -21,8 +38,18 @@ public class BossControl : MonoBehaviour
     {
         if (die)
         {
-            //CAMBIAR ANIMACIOn
+            foreach (GameObject aux in fireballs)
+            {
+                print("alamierda");
+                Destroy(aux);
+            }
+            animator.SetFloat("speed", 1);
+            levelMusic.Stop();
+            endMusic.Play();
             StartCoroutine(dissapear());
+            die = false;
+            shootNow = false;
+            StopCoroutine(shoot());
             //PONERLE MUSIQUILLA
             //TP AL PJ A LA SALA CON EL TOAD
         }
@@ -35,7 +62,9 @@ public class BossControl : MonoBehaviour
     public IEnumerator shoot()
     {
         shootNow = false;
-        Instantiate(prefab, startPos.position, Quaternion.identity).SetActive(true);
+        GameObject aux = Instantiate(prefab, startPos.position, Quaternion.identity);
+        aux.SetActive(true);
+        fireballs.Add(aux);
         yield return new WaitForSeconds(waitTimeBeforeShooting);
         shootNow = true;
     }
@@ -48,7 +77,16 @@ public class BossControl : MonoBehaviour
 
     public IEnumerator dissapear()
     {
-        yield return new WaitForSeconds(10);
+        for (int i = 0; i < 20; i++)
+        {
+            var position = new Vector3(Random.Range(xmin, xmax), Random.Range(ymin, ymax), Random.Range(zmin, zmax));
+            Instantiate(explosion, position, Quaternion.identity).SetActive(true);
+            explosionAudio.Play();
+            yield return new WaitForSeconds(0.5f);
+        }
+
+        SceneManager.LoadScene(0);
+        yield return new WaitForSeconds(5);
         Destroy(this.gameObject);
     }
 
